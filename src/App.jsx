acb9d11e507d6d71e2df25b51e98fa94c7a83767
Denis Sab <denis.sabs@ymail.com>
2024-05-13 16:29:05 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 //import GetBlk from "./Components/GetBlk";
 import FourCards from "./Components/FourCards";
+import HeroSection from "./Components/HeroSection";
+import "./App.css"
 
 function App() {
   return (
     <>
+      <HeroSection />
       <FourCards />
     </>
   );
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div></div>;
-};
+
+  return (
+   <div className="flex justify-between ml-3 mr-3">
+    <img src="src/assets/Place Your Logo Here (Double Click to Edit) 2.png" alt="" />
+    <img src="src/assets/Toolbar Menu.png" alt="" />
+    <img src="src/assets/Button 10.png" alt="" />
+   </div>
+);
+}
 
 export default NavBar;
